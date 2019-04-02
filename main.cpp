60964cd58e9400e812be3ce406e04f31e03594d6
u211-06 <u211-06@public.mpei.local>
2019-04-02 13:59:26 +0300

    New

--- main.cpp
+++ main.cpp
@@ -3,52 +3,76 @@
 
 using namespace std;
 
-int main()
-{
-    size_t number_count=0;
-    cout<<" Enter number count: ";
-    cin>>number_count;
-    vector<double> numbers;
-    numbers.resize(number_count);
-    cout<<" Enter numbers: ";
-    for(size_t i=0; i<number_count; i++)
-    {
-        cin>>numbers[i];
+int
+main() {
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    vector<double> numbers(number_count);
+    for (size_t i = 0; i < number_count; i++) {
+        cin >> numbers[i];
     }
+
     size_t bin_count;
-    cout<<" Enter bin_count: ";
-    cin>>bin_count;
-    vector<size_t> bins(bin_count,0);
-    double min=numbers[0], max=numbers[0];
-    for(double x: numbers)
-    {
-        if(x<min)
-        {
-            min=x;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+    // Обработка данных
+    double min = numbers[0];
+    double max = numbers[0];
+    for (double number : numbers) {
+        if (number < min) {
+            min = number;
         }
-        else if(x>max)
-        {
-            max=x;
+        if (number > max) {
+            max = number;
         }
     }
-    for(double number:numbers)
-    {
-        size_t bin_index=(number-min)*bin_count/(max-min);
-        if(bin_index==bin_count)
-        {
-            bin_index--;
+
+    vector<size_t> bins(bin_count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
         }
-        bins[bin_index]++;
+        bins[bin]++;
     }
-    for(size_t bin: bins)
-    {
-        cout<<bin;
-        cout<<"|";
-        for(size_t i=0; i<bin; i++)
-        {
-            cout<<"*";
+
+    // Вывод данных
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
         }
-        cout<<"\n";
     }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
     return 0;
 }
